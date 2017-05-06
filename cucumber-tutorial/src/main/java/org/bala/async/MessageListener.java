496e21996a3125ae2fb5e45ebd0237197d8d669e
murali <muralibala68@gmail.com>
2017-05-06 20:25:17 +0100

    unit testing of Scheduled executor service added

--- cucumber-tutorial/src/main/java/org/bala/async/MessageListener.java
+++ cucumber-tutorial/src/main/java/org/bala/async/MessageListener.java
@@ -5,7 +5,10 @@ import static java.util.Objects.requireNonNull;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.slf4j.Logger;
@@ -14,16 +17,24 @@ import org.slf4j.LoggerFactory;
 public class MessageListener implements Listener {
     private static final Logger LOG = LoggerFactory.getLogger(MessageListener.class);
 
-    private ExecutorService executorService;
-    private List<Message> cache = new CopyOnWriteArrayList<>(); 
+    private final ExecutorService executorService;
+    private final ScheduledExecutorService scheduledExecutorService;
+    private final List<Message> cache = new CopyOnWriteArrayList<>(); 
 
-    public MessageListener(final ExecutorService executorService) {
+    public MessageListener(final ExecutorService executorService, final ScheduledExecutorService scheduledExecutorService) {
         this.executorService = requireNonNull(executorService);
+        this.scheduledExecutorService = requireNonNull(scheduledExecutorService);
+    }
+    
+    @PostConstruct
+    public void init() {
+        scheduledExecutorService.scheduleWithFixedDelay(() -> checkCacheSize(), 100, 200, TimeUnit.MILLISECONDS);
     }
 
     @PreDestroy
     public void shutdown() {
         executorService.shutdown();
+        scheduledExecutorService.shutdown();
     }
 
     @Override
@@ -34,6 +45,10 @@ public class MessageListener implements Listener {
     private void handleMessage(Message message) {
         cache.add(message);
         LOG.info("Handled message: {}", message);
+    }
+    
+    private void checkCacheSize() {
+        LOG.info("Cache size is {}", cache.size());
     }
     
     public boolean isCacheEmpty() {
--- cucumber-tutorial/src/test/java/org/bala/async/MessageListenerTest.java
+++ cucumber-tutorial/src/test/java/org/bala/async/MessageListenerTest.java
@@ -1,10 +1,14 @@
 package org.bala.async;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,6 +24,7 @@ public class MessageListenerTest {
     private MessageListener messageListener;
     
     @Mock private ExecutorService mockExecutorService;
+    @Mock private ScheduledExecutorService mockScheduledExecutorService;
     @Mock private Message mockMessage;
     
     @Captor private ArgumentCaptor<Runnable> runnableCaptor;
@@ -29,17 +34,23 @@ public class MessageListenerTest {
     @Test
     public void whenExecutorServiceIsNullConstructorToThrow() {
         expectedException.expect(NullPointerException.class);
-        messageListener = new MessageListener(null);
+        messageListener = new MessageListener(null, mockScheduledExecutorService);
     }
     
     @Test
-    public void whenExecutorServiceIsNotNullConstructorNotToThrow() {
-        messageListener = new MessageListener(mockExecutorService);
+    public void whenScheduledExecutorServiceIsNullConstructorToThrow() {
+        expectedException.expect(NullPointerException.class);
+        messageListener = new MessageListener(mockExecutorService, null);
+    }
+    
+    @Test
+    public void whenAllArgumentsAreValidConstructorNotToThrow() {
+        messageListener = new MessageListener(mockExecutorService, mockScheduledExecutorService);
     }
 
     @Test
     public void whenOnMessageIsCalled() {
-        messageListener = new MessageListener(mockExecutorService);
+        messageListener = new MessageListener(mockExecutorService, mockScheduledExecutorService);
         messageListener.onMessage(mockMessage);
         
         verify(mockExecutorService).execute(runnableCaptor.capture());
@@ -47,5 +58,23 @@ public class MessageListenerTest {
         assertThat(messageListener.isCacheEmpty(), is(true));
         runnableCaptor.getValue().run();
         assertThat(messageListener.isCacheEmpty(), is(false));
+    }
+
+    @Test
+    public void whenInitIsCalledScheduledExecutorServiceIsScheduled() {
+        messageListener = new MessageListener(mockExecutorService, mockScheduledExecutorService);
+        messageListener.init();
+        
+        verify(mockScheduledExecutorService).scheduleWithFixedDelay(runnableCaptor.capture(), anyLong(), anyLong(), eq(TimeUnit.MILLISECONDS));
+        runnableCaptor.getValue().run();
+    }
+
+    @Test
+    public void whenShutdownIsCalledAllExecutorServicesAreShutdown() {
+        messageListener = new MessageListener(mockExecutorService, mockScheduledExecutorService);
+        messageListener.shutdown();
+        
+        verify(mockExecutorService).shutdown();
+        verify(mockScheduledExecutorService).shutdown();
     }
 }
